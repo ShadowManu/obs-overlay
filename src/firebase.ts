272722373c4278100f2ef92ab1665edb8f48a779
Manuel Pacheco <contact@manuelpacheco.dev>
2022-04-30 19:01:41 -0400

    setup new firebase project, configuration and security rules

--- src/firebase.ts
+++ src/firebase.ts
@@ -1,12 +1,12 @@
 import { initializeApp } from "firebase/app";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyC5ivdPZWP7y8LUjwfBMPvmHUTePKGjfVs",
-  authDomain: "manuelpachecodev.firebaseapp.com",
-  projectId: "manuelpachecodev",
-  storageBucket: "manuelpachecodev.appspot.com",
-  messagingSenderId: "288872120223",
-  appId: "1:288872120223:web:b1c078f25aa778170647aa",
+  apiKey: "AIzaSyAm9Nko7tFFuKFda8DbH7484mgHXNcXeAs",
+  authDomain: "manuelpachecodev-obsoverlay.firebaseapp.com",
+  projectId: "manuelpachecodev-obsoverlay",
+  storageBucket: "manuelpachecodev-obsoverlay.appspot.com",
+  messagingSenderId: "478338899855",
+  appId: "1:478338899855:web:f27669fefce5b00f4fb5fa",
 };
 
 export const firebase = initializeApp(firebaseConfig);
